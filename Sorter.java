43ce7c2e7969ce66936aa0aa8f154b3322149195
alothmanmj <alothman.maj@gmail.com>
2022-10-02 12:19:07 +0300

    Initial commit

---
+++ Sorter.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Sorter{
+    public static void stringSorter(String[] array){
+
+    }
+
+    public static void main(String[] args) {
+        String [] arr = {"B","A","D","C"};
+
+        stringSorter(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+}
