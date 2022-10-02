7d4f39fac0ad10ef40a2930aab39ccd58ac9fb8e
alothmanmj <alothman.maj@gmail.com>
2022-10-02 12:30:15 +0300

    implemented selection sort in method sort

--- Sorter.java
+++ Sorter.java
@@ -1,8 +1,24 @@
 import java.util.Arrays;
 
 public class Sorter{
-    public static void stringSorter(String[] array){
-        System.out.println("test");
+    public static void stringSorter(String[] arr){
+        int n = arr.length;
+ 
+        // One by one move boundary of unsorted subarray
+        for (int i = 0; i < n-1; i++)
+        {
+            // Find the minimum element in unsorted array
+            int min_idx = i;
+            for (int j = i+1; j < n; j++)
+                if (arr[j].compareTo( arr[min_idx]) < 0)
+                    min_idx = j;
+ 
+            // Swap the found minimum element with the first
+            // element
+            String temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
     }
 
     public static void main(String[] args) {
