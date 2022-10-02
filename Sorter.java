f9e9b1da7d37536c126e2476229a2dd01268441e
alothmanmj <alothman.maj@gmail.com>
2022-10-02 12:33:52 +0300

    test

--- Sorter.java
+++ Sorter.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 public class Sorter{
     public static void stringSorter(String[] array){
         System.out.println("test");
+        
     }
 
     public static void main(String[] args) {
